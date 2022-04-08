97d79b7a63a52679cd020f3bac7a184a14e7541e
omarsalem7 <omarsalem0721@gmail.com>
2022-04-08 23:37:48 +0200

    fix linter errors

--- my_list.rb
+++ my_list.rb
@@ -1,19 +1,19 @@
 require_relative 'enum'
 
 class MyList
-    include MyEnumerable
+  include MyEnumerable
 
-    def initialize(*list)
-        @list = list
-    end
+  def initialize(*list)
+    @list = list
+  end
 
-    def each
-        i = 0
-        while @list[i]
-          yield @list[i]
-          i += 1
-        end
+  def each
+    i = 0
+    while @list[i]
+      yield @list[i]
+      i += 1
     end
+  end
 end
 
 # make instance from MyList
@@ -21,14 +21,10 @@ list = MyList.new(1, 2, 3, 4)
 
 list.all? { |e| e < 5 } # true
 
-
 list.all? { |e| e > 5 } # false
-
 
 list.any? { |e| e == 2 } # true
 
-
 list.any? { |e| e == 5 } # false
-
 
 list.filter(&:even?) # [2, 4]
