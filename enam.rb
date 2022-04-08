e44099a7a7fa4632de196f02318a9054edd61977
Josphat Nkonde <90151551+zairdon20@users.noreply.github.com>
2022-04-08 20:53:39 +0200

    Update enam.rb
    create myEnumarble module

--- enam.rb
+++ enam.rb
@@ -1,1 +1,13 @@
 # our enumberable
+
+module MyEnumerable
+    def any?
+      each { |n| return true if yield n }
+      false
+    end
+    
+    def all?
+        each { |n| return false unless yield n }
+        true
+      end
+end
